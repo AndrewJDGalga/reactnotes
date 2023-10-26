e2381e8e5d26723afc476f23b87bbe59d49a4e09
Andrew DGalga <ex.spake@gmail.com>
2023-10-26 18:32:22 -0500

    notestate. Considered JS class, but that would be a non-react object so holding off for now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-
+import { useState } from "react";
 import Sidebar from "./Sidebar"
 import Editor from "./Editor"
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+        id: 0,
+        title: "New Note",
+        content: "",
+    }]);
+  const [selectedNote, setSelectedNote] = useState(0);
+  const [currentNote, setCurrentNote] = useState(notes[selectedNote]);
+
   return (
     <main>
       <Sidebar />
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,9 @@
 
 export default function Sidebar() {
+    
     return (
-        
+        <aside>
+
+        </aside>
     )
 }
