b64d099a7ec19f4a6ec4baeb76a98e700c86cc14
Andrew DGalga <ex.spake@gmail.com>
2023-10-26 23:09:53 -0500

    simple object blueprint seems to be working just fine for note data. Setup single note.

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { useState } from "react";
 import Sidebar from "./Sidebar"
 import Editor from "./Editor"
 
+class Note {
+  constructor(id, title) {
+    this.id = id,
+    this.title = title,
+    this.content = ""
+  }
+}
+
 function App() {
   const [notes, setNotes] = useState([
-    {
-        id: 0,
-        title: "New Note",
-        content: "",
-    }]);
+    new Note(0, 'New Note'),
+  ]);
   const [selectedNote, setSelectedNote] = useState(0);
   const [currentNote, setCurrentNote] = useState(notes[selectedNote]);
 
   return (
     <main>
-      <Sidebar />
+      <Sidebar noteArray={notes} currentNotePos={selectedNote} />
       <Editor />
     </main>
   )
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,9 +1,14 @@
 
-export default function Sidebar() {
-    
+export default function Sidebar({noteArray, currentNotePos}) {
+    console.log(noteArray[currentNotePos].title);
+
+    const notes = noteArray.map((item)=>{
+        return <h3 key={item.id}>{item.title}</h3>
+    })
+
     return (
         <aside>
-
+            {notes}
         </aside>
     )
 }
