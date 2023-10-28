ed38172baf81e42893278e24e335eb34cb822f5e
Andrew DGalga <ex.spake@gmail.com>
2023-10-27 20:44:35 -0500

    title styling

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -2,11 +2,12 @@
 export default function Editor() {
     return (
         <article className="editor">
+            <input type="text" id="editor-titlewindow" defaultValue="New Note"></input>
             <div className="editor-textstyle">
-                <button>Bold</button>
-                <button>Italic</button>
-                <button>Underline</button>
-                <button>Strikethrough</button>
+                <button id="editor-bold">Bold</button>
+                <button id="editor-italic">Italic</button>
+                <button id="editor-underline">Underline</button>
+                <button id="editor-strikethrough">Strikethrough</button>
             </div>
             <textarea id="editor-textwindow" defaultValue="Enter note here..." rows="0" colums="0"></textarea>
         </article>
