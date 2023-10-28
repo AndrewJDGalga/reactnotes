fc0a51bbae493d14c8561681f48f464960ed2f71
Andrew DGalga <ex.spake@gmail.com>
2023-10-27 20:19:10 -0500

    can't exactly get textarea styled how I want, styling in react is provind a tad frustrating. Got things looking well enough for now.

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,14 +1,14 @@
 
 export default function Editor() {
     return (
-        <article>
-            <div>
+        <article className="editor">
+            <div className="editor-textstyle">
                 <button>Bold</button>
                 <button>Italic</button>
                 <button>Underline</button>
                 <button>Strikethrough</button>
             </div>
-            <textarea defaultValue="Enter note here..."></textarea>
+            <textarea id="editor-textwindow" defaultValue="Enter note here..." rows="0" colums="0"></textarea>
         </article>
     )
 }
